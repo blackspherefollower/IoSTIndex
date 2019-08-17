df7c397b1b59b9b73c8fb890495f1810cc207058
blackspherefollower <darktech@black-sphere.co.uk>
2019-08-17 11:18:39 +0100

    Moving devices.csv into data folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,14 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    `gatsby-transformer-csv`,
+  ],
 }
