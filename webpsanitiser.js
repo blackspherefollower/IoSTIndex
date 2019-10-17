472cda11a5790d8a3b2e8c4ade9c79672ac9e581
blackspherefollower <darktech@black-sphere.co.uk>
2019-10-17 07:33:11 +0100

    feat: More product images

--- webpsanitiser.js
+++ webpsanitiser.js
@@ -1,33 +1,38 @@
-const { exec } = require('child_process');
-const fs = require('fs');
+const { exec } = require(`child_process`)
+const fs = require(`fs`)
 
-exec('find src/data/devices -name "*.webp"', (err, stdout, stderr) => {
+exec(`find src/data/devices -name "*.webp"`, (err, stdout, stderr) => {
   if (err) {
     // node couldn't execute the command
-    return;
+    return
   }
-  let files = stdout.split("\n")
-files.forEach( f => {
-exec(`identify "${f}"`, (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
-  }
-console.log(stdout)
-  if(stdout.includes("JPEG")){
-fs.renameSync(f, f.replace(".webp",".jpeg"))
-} else if(stdout.includes("PNG")){
-fs.renameSync(f, f.replace(".webp",".png"))
-} else if(stdout.includes("WEBP")) {
-exec(`dwebp "${f}" -o "${f.replace(".webp",".png")}"`, (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
-  }
-console.log(stdout, stderr)
-});
-}
-
-});
+  const files = stdout.split(`\n`)
+  files.forEach(f => {
+    exec(`identify "${f}"`, (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        console.error(err)
+        return
+      }
+      console.log(stdout)
+      if (stdout.includes(`JPEG`)) {
+        fs.renameSync(f, f.replace(`.webp`, `.jpeg`))
+      } else if (stdout.includes(`PNG`)) {
+        fs.renameSync(f, f.replace(`.webp`, `.png`))
+      } else if (stdout.includes(`WEBP`)) {
+        exec(
+          `dwebp "${f}" -o "${f.replace(`.webp`, `.png`)}"`,
+          (err, stdout, stderr) => {
+            if (err) {
+              // node couldn't execute the command
+              console.error(err)
+              return
+            }
+            fs.unlinkSync(f)
+            console.log(stdout, stderr)
+          }
+        )
+      }
+    })
+  })
 })
-});
