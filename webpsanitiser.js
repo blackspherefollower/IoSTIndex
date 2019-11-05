86a12520c422ff2191826299f7efe81e58929ca3
blackspherefollower <darktech@black-sphere.co.uk>
2019-11-05 17:10:34 +0000

    chore: Fixing badly named jpegs

--- webpsanitiser.js
+++ webpsanitiser.js
@@ -4,6 +4,7 @@ const fs = require(`fs`)
 exec(`find src/data/devices -name "*.webp"`, (err, stdout, stderr) => {
   if (err) {
     // node couldn't execute the command
+    console.error(err)
     return
   }
   const files = stdout.split(`\n`)
@@ -22,6 +23,43 @@ exec(`find src/data/devices -name "*.webp"`, (err, stdout, stderr) => {
       } else if (stdout.includes(`WEBP`)) {
         exec(
           `dwebp "${f}" -o "${f.replace(`.webp`, `.png`)}"`,
+          (err, stdout, stderr) => {
+            if (err) {
+              // node couldn't execute the command
+              console.error(err)
+              return
+            }
+            fs.unlinkSync(f)
+            console.log(stdout, stderr)
+          }
+        )
+      }
+    })
+  })
+})
+
+exec(`find src/data/devices -name "*.jfif"`, (err, stdout, stderr) => {
+  if (err) {
+    // node couldn't execute the command
+    console.error(err)
+    return
+  }
+  const files = stdout.split(`\n`)
+  files.forEach(f => {
+    exec(`identify "${f}"`, (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        console.error(err)
+        return
+      }
+      console.log(stdout)
+      if (stdout.includes(`JPEG`)) {
+        fs.renameSync(f, f.replace(`.jfif`, `.jpeg`))
+      } else if (stdout.includes(`PNG`)) {
+        fs.renameSync(f, f.replace(`.jfif`, `.png`))
+      } else if (stdout.includes(`WEBP`)) {
+        exec(
+          `dwebp "${f}" -o "${f.replace(`.jfif`, `.png`)}"`,
           (err, stdout, stderr) => {
             if (err) {
               // node couldn't execute the command
