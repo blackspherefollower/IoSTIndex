4c9075f37439f89d4fa3ed9ab940d8331bd5f6b5
blackspherefollower <darktech@black-sphere.co.uk>
2021-11-26 03:39:37 -0800

    feat: Adding more product images

--- webpsanitiser.js
+++ webpsanitiser.js
@@ -1,7 +1,7 @@
 const { exec } = require(`child_process`)
 const fs = require(`fs`)
 
-exec(`find src/data/devices -name "*.webp"`, (err, stdout, stderr) => {
+exec(`find src/data/devices -name "*.avif"`, (err, stdout, stderr) => {
   if (err) {
     // node couldn't execute the command
     console.error(err)
@@ -57,6 +57,57 @@ exec(`find src/data/devices -name "*.jfif"`, (err, stdout, stderr) => {
         fs.renameSync(f, f.replace(`.jfif`, `.jpeg`))
       } else if (stdout.includes(`PNG`)) {
         fs.renameSync(f, f.replace(`.jfif`, `.png`))
+      } else if (stdout.includes(`WEBP`)) {
+        exec(
+          `dwebp "${f}" -o "${f.replace(`.jfif`, `.png`)}"`,
+          (err, stdout, stderr) => {
+            if (err) {
+              // node couldn't execute the command
+              console.error(err)
+              return
+            }
+            fs.unlinkSync(f)
+            console.log(stdout, stderr)
+          }
+        )
+      }
+    })
+  })
+})
+
+
+exec(`find src/data/devices -name "*.avif"`, (err, stdout, stderr) => {
+  if (err) {
+    // node couldn't execute the command
+    console.error(err)
+    return
+  }
+  const files = stdout.split(`\n`)
+  files.forEach(f => {
+    exec(`identify "${f}"`, (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        console.error(err)
+        return
+      }
+      console.log(stdout)
+      if (stdout.includes(`JPEG`)) {
+        fs.renameSync(f, f.replace(`.jfif`, `.jpeg`))
+      } else if (stdout.includes(`PNG`)) {
+        fs.renameSync(f, f.replace(`.jfif`, `.png`))
+      } else if (stdout.includes(`AVIF`)) {
+        exec(
+          `convert "${f}" "${f.replace(`.avif`, `.png`)}"`,
+          (err, stdout, stderr) => {
+            if (err) {
+              // node couldn't execute the command
+              console.error(err)
+              return
+            }
+            fs.unlinkSync(f)
+            console.log(stdout, stderr)
+          }
+        )
       } else if (stdout.includes(`WEBP`)) {
         exec(
           `dwebp "${f}" -o "${f.replace(`.jfif`, `.png`)}"`,
