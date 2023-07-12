d74cb2d506b6fad5c7a2021c3e671dbb1684e9ee
blackspherefollower <darktech@black-sphere.co.uk>
2023-07-12 14:39:12 +0100

    feat: Adding an outlink catcher to AliExpress
    This will popup a model warning dialog instructing users what to look out for.
    
    Fixes #1562

--- src/components/AffiliateLink.js
+++ src/components/AffiliateLink.js
@@ -2,6 +2,100 @@ import React from "react"
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn"
 import LightTooltip from "./LightTooltip"
 
+import Button from "@mui/material/Button"
+import Dialog from "@mui/material/Dialog"
+import DialogActions from "@mui/material/DialogActions"
+import DialogContent from "@mui/material/DialogContent"
+import DialogContentText from "@mui/material/DialogContentText"
+import DialogTitle from "@mui/material/DialogTitle"
+import { navigate } from "gatsby-link"
+import {
+  LazyLoadImage,
+} from "react-lazy-load-image-component"
+import aliExample from'./ali-example.png';
+
+function AliDialog(props) {
+  const [open, setOpen] = React.useState(false)
+  const [newTab, setNewTab] = React.useState(false)
+
+  const handleClickOpen = (e) => {
+    e.preventDefault()
+    setOpen(true)
+  }
+  const handleClickAltOpen = (e) => {
+    e.preventDefault()
+    setNewTab(true)
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+    if (newTab) {
+      window.open(props.href)
+    } else {
+      window.location.href = props.href
+    }
+  }
+
+  return (
+    <div>
+      <a
+        onClick={handleClickOpen}
+        onAuxClick={handleClickAltOpen}
+        href={props.href}
+        title={props.title}
+      >
+        {props.href}
+      </a>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {`Going to AliExpress?`}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            AliExpress is great, but there's some things you should be aware of
+            before you buy!
+            <ul>
+              <li>
+                Sometimes AliExpress listings are reused and no longer reference
+                the product that IoSTIndex is linking to. If the images don't
+                look the same, there's a good chance the product has changed.
+                Please let me kow if you spot this!
+              </li>
+              <li>
+                Many AliExpress listings use the color picker to list very
+                different devices. Sometimes it is the color or how it's
+                packaged, but it's really common to find cheaper devices listed
+                as options to bring the overall listing price down.{` `}
+                <b>Always</b> check that the device you add to your cart is the
+                app enabled model you expect. If in doubt, come find me on{` `}
+                <a
+                  href="https://discord.buttplug.io"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  discord
+                </a>
+                {` `}
+                and I'll help!
+              </li>
+            </ul>
+            <LazyLoadImage src={aliExample} width="100%" />
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Continue to AliExpress</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
+
 export default function AffiliateLink(props) {
   const device = props.device
 
@@ -20,16 +114,27 @@ export default function AffiliateLink(props) {
 
   const hasUrl = device.Detail.length > 0
   const hasAUrl = device.Affiliate_Link.length > 0
+  const isAli =
+    device.Detail.includes(`aliexpress`) ||
+    device.Affiliate_Link.includes(`aliexpress`)
 
   if (hasAUrl) {
     return (
       <div style={{ display: `flex`, alignItems: `center` }}>
-        <a
-          href={device.Affiliate_Link}
-          title={`Affiliate link: ${device.Brand} - ${device.Device}`}
-        >
-          {device.Affiliate_Link}
-        </a>
+        {isAli && (
+          <AliDialog
+            href={device.Detail}
+            title={`Product link: ${device.Brand} - ${device.Device}`}
+          />
+        )}
+        {!isAli && (
+          <a
+            href={device.Detail}
+            title={`Product link: ${device.Brand} - ${device.Device}`}
+          >
+            {device.Detail}
+          </a>
+        )}
         <LightTooltip
           interactive
           title={
@@ -41,12 +146,20 @@ export default function AffiliateLink(props) {
                 <span>
                   {` `}
                   The direct link to the product is:{` `}
-                  <a
-                    href={device.Detail}
-                    title={`Product link: ${device.Brand} - ${device.Device}`}
-                  >
-                    {device.Detail}
-                  </a>
+                  {isAli && (
+                    <AliDialog
+                      href={device.Detail}
+                      title={`Product link: ${device.Brand} - ${device.Device}`}
+                    />
+                  )}
+                  {!isAli && (
+                    <a
+                      href={device.Detail}
+                      title={`Product link: ${device.Brand} - ${device.Device}`}
+                    >
+                      {device.Detail}
+                    </a>
+                  )}
                 </span>
               )}
             </React.Fragment>
@@ -54,6 +167,15 @@ export default function AffiliateLink(props) {
         >
           <MonetizationOnIcon />
         </LightTooltip>
+      </div>
+    )
+  } else if (hasUrl && isAli) {
+    return (
+      <div>
+        <AliDialog
+          href={device.Detail}
+          title={`Product link: ${device.Brand} - ${device.Device}`}
+        />
       </div>
     )
   } else if (hasUrl) {
